84908ba891de2f1c2ad075cb2b207fbdfa1925a3
topTalent1212 <topone1212talent@gmail.com>
2019-10-22 22:02:03 +0800

    Update tested version with single token set at a time.

--- contracts/PortfolioManager.sol
+++ contracts/PortfolioManager.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.5.0;
 
-import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "./Uniswap/UniswapExchangeInterface.sol";
+import "./Uniswap/UniswapFactoryInterface.sol";
 
 
 contract PortfolioManager is Ownable {
@@ -14,16 +14,11 @@ contract PortfolioManager is Ownable {
     address payable recipient;
     uint8 constant HUNDRED_PERCENT = 100;
     uint8 constant SWAP_THRESHOLD = 95;
+    uint256 deadline = 1603374468000;
     mapping(address=>uint8) percentages;
 
-    event PortfolioUpdate(
-        address indexed account,
-        address[] tokens,
-        uint8[] percentages
-    );
-
-    constructor(address _uniswap) public {
-        factory = UniswapFactoryInterface(_uniswap);
+    constructor(address _factory) public {
+        factory = UniswapFactoryInterface(_factory);
         recipient = msg.sender;
     }
 
@@ -33,23 +28,16 @@ contract PortfolioManager is Ownable {
             uint256 amountETH = msg.value.mul(percentages[token]).div(HUNDRED_PERCENT);
             UniswapExchangeInterface exchange = UniswapExchangeInterface(factory.getExchange(token));
             uint256 minToken = exchange.getEthToTokenInputPrice(amountETH).mul(SWAP_THRESHOLD).div(100);
-            exchange.ethToTokenTransferInput.value(amountETH)(minToken, 0, owner());
+            exchange.ethToTokenTransferInput.value(amountETH)(minToken, deadline, owner());
         }
-        uint256 remaining_eth = address(this).balance;
-        recipient.transfer(remaining_eth);
+        uint256 remaining = address(this).balance;
+        recipient.transfer(remaining);
     }
 
-    function setPortfolio(address[] memory _tokens, uint8[] memory _percentages ) public returns (bool success) {
-        require(_tokens.length == _percentages.length, "Use arrays of same length for updateing");
-        clearPortfolio();
-        uint256 percentageSum = 0;
-        for (uint8 i = 0; i < _tokens.length; i ++) {
-            percentageSum = percentageSum.add(_percentages[i]);
-            require(percentageSum <= HUNDRED_PERCENT, "Invalid percentages");
-            investments[i] = _tokens[i];
-            percentages[_tokens[i]] = _percentages[i];
-        }
-        emit PortfolioUpdate(msg.sender, _tokens, _percentages);
+    function setPortfolio(address _token, uint8 _percentage ) public returns (bool success) {
+        require(factory.getExchange(_token) != address(0), "Token has no exchange yet.");
+        investments.push(_token);
+        percentages[_token] = _percentage;
         return true;
     }
 
