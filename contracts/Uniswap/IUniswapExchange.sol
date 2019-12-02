697939c0d10f499ec89ff585c8322c8dbf67dccc
topTalent1212 <topone1212talent@gmail.com>
2019-12-02 11:39:50 +0800

    Add addLiquidity in UniswapExchange Interface

--- contracts/Uniswap/IUniswapExchange.sol
+++ contracts/Uniswap/IUniswapExchange.sol
@@ -2,6 +2,8 @@ pragma solidity >=0.5.0 <0.6.0;
 
 
 interface IUniswapExchange {
+    function addLiquidity(uint256 min_liquidity, uint256 max_tokens, uint256 deadline) external payable returns (uint256);
+
     // Get Prices
     function getEthToTokenInputPrice(uint256 eth_sold) external view returns (uint256 tokens_bought);
     function getEthToTokenOutputPrice(uint256 tokens_bought) external view returns (uint256 eth_sold);
