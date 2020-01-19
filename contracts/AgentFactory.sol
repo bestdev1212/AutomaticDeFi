f761c43fb68a16637d550ddf42b5aba1c66d3ac8
topTalent1212 <topone1212talent@gmail.com>
2020-01-19 14:58:14 +0800

    Update clone address to payable because we have a fullback function in ReceiveAgent

--- contracts/AgentFactory.sol
+++ contracts/AgentFactory.sol
@@ -1,22 +1,22 @@
 pragma solidity ^0.5.0;
 
 import "./CloneFactory.sol";
-// import "./ReceiveAgent.sol";
+import "./ReceiveAgent.sol";
 
 
 contract AgentFactory is CloneFactory {
 
-    address public agentAddress;
+    address public agentImplementation;
 
     event AgentCreated(address newAgentAddress);
 
-    constructor(address _agentAddress) public {
-        agentAddress = _agentAddress;
+    constructor(address _implementation) public {
+        agentImplementation = _implementation;
     }
 
-    function createAgent() public {
-        address clone = createClone(agentAddress);
-        // ReceiveAgent(clone);
+    function createAgent() public returns (address payable clone) {
+        clone = createClone(agentImplementation);
+        ReceiveAgent(clone).init(msg.sender);
         emit AgentCreated(clone);
     }
 }
